44508bce7141d6be1a8bf36a93fc73dbad8942b8
q210138 <a.porcu@student.helmo.be>
2024-10-08 13:48:36 +0200

    Created and implemented for reading csv files

---
+++ domains/src/main/java/org/helmo/example/domains/rooms/CanReadRooms.java
@@ -0,0 +1,10 @@
+package org.helmo.example.domains.rooms;
+
+import org.helmo.example.domains.core.Room;
+
+import java.util.List;
+
+
+public interface CanReadRooms {
+    List<Room> getRooms();
+}
---
+++ infrastructures/src/main/java/org/helmo/example/infrastructures/RoomFileRepository.java
@@ -0,0 +1,40 @@
+/*
+ * This source file is an example
+ */
+package org.helmo.example.infrastructures;
+
+import org.helmo.example.domains.core.Room;
+import org.helmo.example.domains.rooms.CanReadRooms;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomFileRepository implements CanReadRooms {
+
+    private String filePath;
+
+    public RoomFileRepository(String path) {
+        filePath = path;
+    }
+
+    @Override
+    public List<Room> getRooms() {
+        List<Room> rooms = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                var values = line.split(";");
+
+                rooms.add(new Room(values[0],values[1],Integer.parseInt(values[2])));
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return rooms;
+    }
+}
