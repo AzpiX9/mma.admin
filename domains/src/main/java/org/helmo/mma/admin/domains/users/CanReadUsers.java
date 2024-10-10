bbef2790a1dc627fcf62fd0f13dbcefda1af6ccd
q210138 <a.porcu@student.helmo.be>
2024-10-10 17:35:36 +0200

    refactored for getting user by matricule

--- domains/src/main/java/org/helmo/mma/admin/domains/users/CanReadUsers.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/users/CanReadUsers.java
@@ -3,7 +3,10 @@ package org.helmo.mma.admin.domains.users;
 import org.helmo.mma.admin.domains.core.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CanReadUsers {
-    List<User> getUsers();
+    Map<String,User> getUsers();
+
+    User getUser(String matricule);
 }
--- infrastructures/src/main/java/org/helmo/mma/admin/infrastructures/UserFileRepository.java
+++ infrastructures/src/main/java/org/helmo/mma/admin/infrastructures/UserFileRepository.java
@@ -7,30 +7,40 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserFileRepository implements CanReadUsers {
     private final String filePath;
+    //private final List<User> users = new ArrayList<>();
+    private final Map<String,User> usersM = new HashMap<>();
+
 
     public UserFileRepository(String path) {
         filePath = path;
     }
 
+    //List ou Map ? pour stocker et récupérer les users
     @Override
-    public List<User> getUsers() {
-        List<User> users = new ArrayList<>();
+    public Map<String,User> getUsers() {
         try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null){
                 var values = line.split(";");
-
-                users.add(new User(values[0],values[1],values[2],values[3]));
+                var userTemp = new User(values[0],values[1],values[2],values[3]);
+                //this.users.add(userTemp);
+                this.usersM.put(values[0],userTemp);
             }
-
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        return users;
+        return this.usersM;
+    }
+
+    @Override
+    public User getUser(String matricule) {
+        return usersM.get(matricule);
     }
 }
