e5972fcf97b87f30cf698a36005ecdcd4ef0567d
q210138 <a.porcu@student.helmo.be>
2024-10-10 17:34:30 +0200

    Created repo for managing booking events

---
+++ domains/src/main/java/org/helmo/mma/admin/domains/booking/CalendarRepository.java
@@ -0,0 +1,9 @@
+package org.helmo.mma.admin.domains.booking;
+
+import org.helmo.mma.admin.domains.core.Booking;
+
+import java.util.List;
+
+public interface CalendarRepository extends CanReadBooked, CanWriteBooked {
+
+}
