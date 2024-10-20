e129de101e66fa67605ff52d06233543cace8e06
q210138 <a.porcu@student.helmo.be>
2024-10-20 12:02:13 +0200

    Exception for calendar repo side

---
+++ domains/src/main/java/org/helmo/mma/admin/domains/exceptions/CalendarException.java
@@ -0,0 +1,7 @@
+package org.helmo.mma.admin.domains.exceptions;
+
+public class CalendarException extends RuntimeException {
+    public CalendarException(String message) {
+        super(CalendarException.class.getName()+" -> "+message);
+    }
+}
