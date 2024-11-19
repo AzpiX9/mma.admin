8bc586acc915581af70bb95e10165d5db9b175cd
q210138 <a.porcu@student.helmo.be>
2024-11-19 16:41:55 +0100

    Added moved method from Presenter

--- domains/src/main/java/org/helmo/mma/admin/domains/core/BookingAggregator.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/core/BookingAggregator.java
@@ -1,9 +1,12 @@
 package org.helmo.mma.admin.domains.core;
 
 import org.helmo.mma.admin.domains.booking.CalendarRepository;
+import org.helmo.mma.admin.domains.booking.EventUtils;
 import org.helmo.mma.admin.domains.rooms.CanReadRooms;
 import org.helmo.mma.admin.domains.users.CanReadUsers;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 public class BookingAggregator {
@@ -29,5 +32,33 @@ public class BookingAggregator {
 
     public CanReadUsers getUsersRepo() {
         return usersRepo;
+    }
+
+    public String checkIfNotValid(Booking booking) {
+        var collision = calendarRepository.getBookingsBy(booking.IdSalle(), booking.JourReservation())
+                .stream().anyMatch(ev -> ev.Debut().isBefore(booking.Fin()) && booking.Debut().isBefore(ev.Fin()) );
+        if(collision){
+            return "Crénau occupé";
+        }
+        if(booking.NbPersonnes() > roomsRepo.getRoom(booking.IdSalle()).Size()){
+            return "Capacité insuffisante";
+        }
+        if(!usersRepo.exists(booking.Matricule())){
+            return "Utilisateur non trouvé";
+        }
+
+        return "";
+    }
+
+    public User getUserFromMatricule(String matricule) {
+        return usersRepo.getUser(matricule);
+    }
+
+    public List<Room> getRooms(){
+        return roomsRepo.getRooms();
+    }
+
+    public List<String> eventsToString(LocalDate dateGiven, String roomId){
+        return EventUtils.transform(calendarRepository.getBookingsBy(roomId,dateGiven));
     }
 }
