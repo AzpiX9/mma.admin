6bae379aba3a665642a4dc1dc2f452e743506efa
q210138 <a.porcu@student.helmo.be>
2024-11-19 16:39:45 +0100

    Made to a record

--- infrastructures/src/main/java/org/helmo/mma/admin/infrastructures/BookingDTO.java
+++ infrastructures/src/main/java/org/helmo/mma/admin/infrastructures/BookingDTO.java
@@ -5,41 +5,13 @@ import org.helmo.mma.admin.domains.core.Booking;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-public class BookingDTO {
-
-    private String matricule;
-    private String salle;
-    private ZonedDateTime debut;
-    private ZonedDateTime fin;
-    private String description;
+public record BookingDTO(String Matricule, String Salle, ZonedDateTime Debut, ZonedDateTime Fin, String Description) {
 
     public BookingDTO(Booking booking) {
-
-        this.matricule = booking.Matricule();
-        this.salle = booking.IdSalle();
-        this.debut = ZonedDateTime.of( booking.JourReservation(),booking.Debut(),ZoneId.systemDefault());
-        this.fin = ZonedDateTime.of(booking.JourReservation(),booking.Fin(),ZoneId.systemDefault());
-        this.description = booking.Description();
+        this(booking.Matricule(),
+                booking.IdSalle(),
+                ZonedDateTime.of(booking.JourReservation(), booking.Debut(), ZoneId.systemDefault()),
+                ZonedDateTime.of(booking.JourReservation(), booking.Fin(), ZoneId.systemDefault()),
+                booking.Description());
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getMatricule() {
-        return matricule;
-    }
-
-    public String getSalle() {
-        return salle;
-    }
-
-    public ZonedDateTime getDebut() {
-        return debut;
-    }
-
-    public ZonedDateTime getFin() {
-        return fin;
-    }
-
 }
