6b13631524dcf8b96d11b42d2303855de4e64737
q210138 <a.porcu@student.helmo.be>
2024-11-20 17:00:01 +0100

    Refactored

--- domains/src/main/java/org/helmo/mma/admin/domains/exceptions/BookingException.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/exceptions/BookingException.java
@@ -2,6 +2,6 @@ package org.helmo.mma.admin.domains.exceptions;
 
 public class BookingException extends RuntimeException {
     public BookingException(String message) {
-        super("BookingException -> "+message);
+        super(message);
     }
 }
