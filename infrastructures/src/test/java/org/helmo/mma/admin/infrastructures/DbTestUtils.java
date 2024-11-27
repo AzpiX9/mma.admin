f313fcfa2dba0120256ba40b9ff2777266455ae8
q210138 <a.porcu@student.helmo.be>
2024-11-27 15:11:57 +0100

    utils for testing db queries

---
+++ infrastructures/src/test/java/org/helmo/mma/admin/infrastructures/DbTestUtils.java
@@ -0,0 +1,43 @@
+package org.helmo.mma.admin.infrastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbTestUtils {
+
+    public static List<String> insertAllRooms(){
+        var roomAddQuery = new ArrayList<String>();
+        roomAddQuery.add("CREATE TABLE Rooms(idRoom VARCHAR PRIMARY KEY,roomName VARCHAR,roomSize INTEGER NOT NULL);");
+        roomAddQuery.add("INSERT INTO Rooms(idRoom, roomName, roomSize)VALUES ('LC1', 'Learning Center 1', 20);");
+        roomAddQuery.add("INSERT INTO Rooms(idRoom, roomName, roomSize)VALUES ('LC2', 'Learning Center 2', 20);");
+        roomAddQuery.add("INSERT INTO Rooms(idRoom, roomName, roomSize)VALUES ('LB1', 'Learning Box 1', 5);");
+        roomAddQuery.add("INSERT INTO Rooms(idRoom, roomName, roomSize)VALUES ('LB2', 'Learning Box 2', 5);");
+        return roomAddQuery;
+    }
+
+    public static List<String> insertAllServices(){
+        var servicesAddQuery = new ArrayList<String>();
+        servicesAddQuery.add("CREATE TABLE Services(idService INTEGER PRIMARY KEY,description VARCHAR);");
+        servicesAddQuery.add("INSERT INTO Services(idService, description)VALUES (1, 'Cafe et boissons');");
+        servicesAddQuery.add("INSERT INTO Services(idService, description)VALUES (2, 'Nourriture');");
+        servicesAddQuery.add("INSERT INTO Services(idService, description)VALUES (3, 'Projecteur');");
+        servicesAddQuery.add("INSERT INTO Services(idService, description)VALUES (4, 'Matériel Sonore');");
+        return servicesAddQuery;
+    }
+
+    public static List<String> insertAllMembers(){
+        var memberAddQuery = new ArrayList<String>();
+        memberAddQuery.add("CREATE TABLE Member(userMatr VARCHAR PRIMARY KEY,userFullName VARCHAR,userMail VARCHAR);");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('A123456', 'Jean Dupont', 'j.dupont@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('B234567', 'Marie Dubois', 'm.dubois@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('C345678', 'Pierre Martin', 'p.martin@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('D456789', 'Sophie Lambert', 's.lmabert@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('E567890', 'Luc Lefevre', 'l.lefevre@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('F678901', 'Claire Simon', 'c.simon@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('G789012', 'Marc Durand', 'm.durand@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('H890123', 'Julie Bernard', 'j.bernard@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('I901234', 'Paul Richard', 'p.richard@helmo.be');");
+        memberAddQuery.add("INSERT INTO Member(userMatr, userFullName, userMail)VALUES ('J012345', 'Emma Petit', 'e.petit@helmo.be');");
+        return memberAddQuery;
+    }
+}
