7c902f0d5e93d9ed7480efd152c5190aaccee126
q210138 <a.porcu@student.helmo.be>
2024-11-28 15:33:36 +0100

    Refactored

--- domains/src/main/java/org/helmo/mma/admin/domains/booking/CanReadBooked.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/booking/CanReadBooked.java
@@ -2,16 +2,12 @@ package org.helmo.mma.admin.domains.booking;
 
 import org.helmo.mma.admin.domains.core.LocalEvent;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Map;
 
 public interface CanReadBooked {
     void readTo();
 
-    List<LocalEvent> retrieveAll(); //TODO: ajouter une date
+    Map<String,LocalEvent> retrieveAll(); //TODO: ajouter une date
 
-    LocalEvent getBooking(String idRoom, LocalDateTime slotDT);
 
-    List<LocalEvent> getBookingsBy(String location, LocalDate date);
 }
