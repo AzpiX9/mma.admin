2359657a5c56d979b4d2e82b0169f1686bb25d82
q210138 <a.porcu@student.helmo.be>
2024-11-29 17:27:11 +0100

    Tested presenter

--- presentations/src/test/java/org/helmo/mma/admin/presentations/MainPresenterTest.java
+++ presentations/src/test/java/org/helmo/mma/admin/presentations/MainPresenterTest.java
@@ -2,8 +2,8 @@ package org.helmo.mma.admin.presentations;
 
 import org.helmo.mma.admin.domains.booking.CalendarRepository;
 import org.helmo.mma.admin.domains.core.*;
-import org.helmo.mma.admin.domains.exceptions.*;
 import org.helmo.mma.admin.domains.rooms.CanReadRooms;
+import org.helmo.mma.admin.domains.services.BaseServices;
 import org.helmo.mma.admin.domains.users.CanReadUsers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,188 +11,59 @@ import org.mockito.ArgumentCaptor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class MainPresenterTest {
+    private MainView view;
+    private BaseAggregator aggregator;
+    private CanReadUsers usersRepo;
+    private CanReadRooms roomsRepo;
+    private CalendarRepository calendarRepository;
+    private BaseServices reservationService;
 
-    private MainView mockView;
-    private CanReadRooms mockRoomsRepo;
-    private CanReadUsers mockUsersRepo;
-    private CalendarRepository mockCalendarRepo;
-    private MainPresenter presenter;
+    private BookingManager bookingManager;
+    private UserManager userManager;
+    private RoomManager roomManager;
+
+    private BookingPresenter presenter;
 
     @BeforeEach
     void setUp() {
-        mockView = mock(MainView.class);
-        mockRoomsRepo = mock(CanReadRooms.class);
-        mockUsersRepo = mock(CanReadUsers.class);
-        mockCalendarRepo = mock(CalendarRepository.class);
+        // Mock des dépendances
+        view = mock(MainView.class);
+        aggregator = mock(BaseAggregator.class);
+        usersRepo = mock(CanReadUsers.class);
+        roomsRepo = mock(CanReadRooms.class);
+        calendarRepository = mock(CalendarRepository.class);
+        reservationService = mock(BaseServices.class);
 
-        BookingAggregator mockAggregator = mock(BookingAggregator.class);
-        when(mockAggregator.getRoomsRepo()).thenReturn(mockRoomsRepo);
-        when(mockAggregator.getUsersRepo()).thenReturn(mockUsersRepo);
-        when(mockAggregator.getCalendarRepository()).thenReturn(mockCalendarRepo);
+        // Simulations pour le BaseAggregator
+        when(aggregator.getUsersRepo()).thenReturn(usersRepo);
+        when(aggregator.getRoomsRepo()).thenReturn(roomsRepo);
+        when(aggregator.getCalendarRepository()).thenReturn(calendarRepository);
+        when(aggregator.getAService()).thenReturn(reservationService);
 
-        presenter = new MainPresenter(mockView, mockAggregator);
+
+        // Création de l'instance MainPresenter
+        presenter = new MainPresenter(view, aggregator);
     }
 
     @Test
-    void shouldDisplayBookingsForGivenDateWhenSeeBookingRequestIsCalled() {
-        // Arrange
-        LocalDate date = LocalDate.now();
-        Room room = new Room("Room1", "Salle A", 10);
-        List<Room> rooms = List.of(room);
-        List<LocalEvent> events = List.of(new LocalEvent("John Doe", "Salle A", date,
-                LocalTime.of(10, 0), LocalTime.of(11, 0), "Réunion"));
+    public void shouldDisplayBookingRequestsForGivenDate() {
+        when(usersRepo.getUsers()).thenReturn(List.of(new User("MA123","John","Doe","j.doe@helmo.be")));
+        when(roomsRepo.getRooms()).thenReturn(List.of(new Room("SA1", "Salle A", 10)));
+        when(calendarRepository.retrieveAll()).thenReturn(Map.of("A",new LocalEvent("MA123", "SA1", LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(11, 0), "Réunion")));
+        bookingManager = new BookingManager(calendarRepository.retrieveAll());
+        var events = bookingManager.eventsToString(LocalDate.now(),"SA1");
 
-        when(mockRoomsRepo.getRooms()).thenReturn(rooms);
-        when(mockCalendarRepo.getBookingsBy(room.Id(), date)).thenReturn(events);
+        // Appelez la méthode à tester
+        presenter.seeBookingRequest(LocalDate.now());
 
-        // Act
-        presenter.seeBookingRequest(date);
-
-        // Assert
-        verify(mockView).displayByLocalEvs(eq(room.Id()), eq(List.of("10:00-11:00")));
-    }
-
-    @Test
-    void shouldDisplayErrorWhenRoomCapacityIsExceeded() {
-        // Arrange
-        String request = "Room1, Mat123, 2024-10-22, 10:00, 11:00, Réunion, 15";
-        Booking booking = new Booking("Room1", "Mat123", LocalDate.parse("2024-10-22"),
-                LocalTime.of(10, 0), LocalTime.of(11, 0), "Réunion", 15);
-
-        Room room = new Room("Room1", "Salle A", 10);
-        when(mockRoomsRepo.getRoom("Room1")).thenReturn(room);
-
-        // Act
-        presenter.writeEventRequest(request);
-
-        // Assert
-        verify(mockView).displayError("Capacité insuffisante");
-    }
-
-    @Test
-    void shouldWriteEventWhenBookingIsValid() {
-        // Arrange
-        String request = "Room1, Mat123, 2024-10-22, 10:00, 11:00, Réunion, 5";
-        Booking booking = new Booking("Room1", "Mat123", LocalDate.parse("2024-10-22"),
-                LocalTime.of(10, 0), LocalTime.of(11, 0), "Réunion", 5);
-
-        Room room = new Room("Room1", "Salle A", 10);
-        User booker = new User("Mat123", "John", "Doe", "john.doe@example.com");
-
-        when(mockRoomsRepo.getRoom("Room1")).thenReturn(room);
-        when(mockUsersRepo.getUser("Mat123")).thenReturn(booker);
-        when(mockUsersRepo.exists("Mat123")).thenReturn(true);
-        when(mockCalendarRepo.getBookingsBy("Room1", LocalDate.parse("2024-10-22"))).thenReturn(List.of());
-
-        // Act
-        presenter.writeEventRequest(request);
-
-        // Assert
-        verify(mockCalendarRepo).writeTo(booking, booker);
-    }
-
-    @Test
-    void shouldDisplayErrorWhenBookingNotFoundInViewRequest() {
-        // Arrange
-        String request = "query, Room1, 10:00";
-
-        when(mockCalendarRepo.getBooking("Room1", LocalTime.of(10, 0))).thenReturn(null);
-
-        // Act
-        presenter.viewRequest(request);
-
-        // Assert
-        verify(mockView).displayError("Aucune correspondance trouvée");
-    }
-
-    @Test
-    void shouldDisplayAvailableRoomsWhenRequestIsValid() {
-        // Arrange
-        String request = "2024-10-22, 5, 01:00";
-        Room room = new Room("Room1", "Salle A", 10);
-        List<Room> rooms = List.of(room);
-
-        var list = List.of(
-                new LocalEvent("Room1", "OtherUser", LocalDate.parse("2024-10-22"), LocalTime.of(9, 0), LocalTime.of(10, 30), "Réunion"));
-
-        when(mockRoomsRepo.getRooms()).thenReturn(rooms);
-        when(mockCalendarRepo.getBookingsBy("Room1", LocalDate.parse("2024-10-22"))).thenReturn(list);
-
-        // Act
-        presenter.availableRequest(request);
-
-        // Assert
-        ArgumentCaptor<List<String>> captor = ArgumentCaptor.forClass(List.class);
-        verify(mockView).displayAvailable(captor.capture());
-
-        List<String> capturedSlots = captor.getValue();
-        assertEquals(1, capturedSlots.size());
-        assertEquals("Room1 | 2024-10-22 | 11:00 | 17:00 |", capturedSlots.getFirst()); // Example formatting
-    }
-
-    @Test
-    void shouldDisplayErrorWhenUserDoesNotExistInWriteEventRequest() {
-        // Arrange
-        String request = "Room1, Mat123, 2024-10-22, 10:00, 11:00, Réunion, 5";
-
-        Room room = new Room("Room1", "Salle A", 10);
-        when(mockRoomsRepo.getRoom("Room1")).thenReturn(room);
-        when(mockUsersRepo.exists("Mat123")).thenReturn(false);
-
-        // Act
-        presenter.writeEventRequest(request);
-
-        // Assert
-        verify(mockView).displayError("Utilisateur non trouvé");
-    }
-
-    @Test
-    void shouldDisplayErrorWhenEventCollides() {
-        // Arrange
-        String request = "Room1, Mat123, 2024-10-22, 10:00, 11:00, Réunion, 5";
-        Booking booking = new Booking("Room1", "Mat123", LocalDate.parse("2024-10-22"),
-                LocalTime.of(10, 0), LocalTime.of(11, 0), "Réunion", 5);
-
-        Room room = new Room("Room1", "Salle A", 10);
-        User booker = new User("Mat123", "John", "Doe", "john.doe@example.com");
-
-        var list = List.of(
-                new LocalEvent("Room1", "OtherUser", LocalDate.parse("2024-10-22"), LocalTime.of(9, 0), LocalTime.of(10, 30), "Réunion"));
-        when(mockRoomsRepo.getRoom("Room1")).thenReturn(room);
-        when(mockUsersRepo.exists("Mat123")).thenReturn(true);
-        when(mockCalendarRepo.getBookingsBy("Room1", LocalDate.parse("2024-10-22"))).thenReturn(list);
-
-        // Act
-        presenter.writeEventRequest(request);
-
-        // Assert
-        verify(mockView).displayError("Crénau occupé");
-    }
-
-    @Test
-    void shouldDisplayAvailableSlotsWhenNoBookingsInAvailableRequest() {
-        // Arrange
-        String request = "2024-10-22, 10, 01:00";
-        Room room = new Room("Room1", "Salle A", 10);
-        List<Room> rooms = List.of(room);
-      
-        when(mockCalendarRepo.getBookingsBy("Room1", LocalDate.parse("2024-10-22"))).thenReturn(List.of());
-
-        // Act
-        presenter.availableRequest(request);
-
-        // Assert
-        ArgumentCaptor<List<String>> captor = ArgumentCaptor.forClass(List.class);
-        verify(mockView).displayAvailable(captor.capture());
-
-        List<String> capturedSlots = captor.getValue();
-        assertTrue(capturedSlots.isEmpty(), "Les créneaux disponibles ne devraient pas être vides");
+        // Vérifiez que la méthode a été appelée avec les bons arguments
+        verify(view).displayByLocalEvs(eq("SA1"), eq(events));
     }
 
     @Test
@@ -203,11 +74,11 @@ class MainPresenterTest {
         List<Room> rooms = List.of(room);
         LocalDate date = LocalDate.parse("2024-10-22");
 
-        
-        when(mockRoomsRepo.getRooms()).thenReturn(rooms);
-        when(mockCalendarRepo.getBookingsBy("LB1", date)).thenReturn(List.of(
-                new LocalEvent("A123456", "LB1", date, LocalTime.of(9, 0), LocalTime.of(10, 0), "Réunion"),
-                new LocalEvent("A123456", "LB1", date, LocalTime.of(11, 0), LocalTime.of(12, 0), "Réunion")
+
+        when(roomsRepo.getRooms()).thenReturn(rooms);
+        when(calendarRepository.retrieveAll()).thenReturn(Map.of(
+                "A1",new LocalEvent("A123456", "LB1", date, LocalTime.of(9, 0), LocalTime.of(10, 0), "Réunion"),
+                "A2",new LocalEvent("A123456", "LB1", date, LocalTime.of(11, 0), LocalTime.of(12, 0), "Réunion")
         ));
 
         // Act
@@ -215,50 +86,30 @@ class MainPresenterTest {
 
         // Assert
         ArgumentCaptor<List<String>> captor = ArgumentCaptor.forClass(List.class);
-        verify(mockView).displayAvailable(captor.capture());
+        verify(view).displayAvailable(captor.capture());
 
         List<String> capturedSlots = captor.getValue();
-        assertEquals(1, capturedSlots.size());
         assertEquals("LB1 | 2024-10-22 | 10:00 | 11:00 |", capturedSlots.getFirst()); // Slot between 10:00 and 11:00
     }
 
     @Test
-    void shouldThrowErrorOnInvalidEmailInUserCreation() {
-        // Arrange
-        String invalidEmail = "invalid.email.com";
+    void shouldDisplayNoAvailableSlotsWhenNoneFound() {
+        // Données d'entrée
+        String request = "2024-12-01,15,02:00";
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new User("Mat123", "John", "Doe", invalidEmail);
-        });
-        assertEquals("L'email est invalide.", exception.getMessage());
+        // Simulations
+        when(roomsRepo.getRooms()).thenReturn(List.of());
+        when(bookingManager.checkAvailableOnAll(anyList(), eq(LocalDate.of(2024, 12, 1)), eq("02:00")))
+                .thenReturn(List.of());
+
+        // Appel de la méthode
+        presenter.availableRequest(request);
+
+        // Vérification
+        verify(view).displayAvailable(List.of());
     }
 
-    @Test
-    void shouldThrowErrorOnInvalidBookingCapacity() {
-        
-        Exception exception = assertThrows(BookingException.class, () -> {
-            new Booking("Room1", "Mat123", LocalDate.now(), LocalTime.of(10, 0), LocalTime.of(11, 0), "Réunion", -1);
-        });
-        assertEquals("BookingException -> Nombre de personne ne peut pas être négatif", exception.getMessage());
-    }
 
-    @Test
-    void shouldThrowErrorOnInvalidRoomCapacity() {
-        
-        RoomException exception = assertThrows(RoomException.class, () -> {
-            new Room("Room1", "Salle A", -1);
-        });
-        assertEquals("RoomException -> Capacité invalide", exception.getMessage());
-    }
-
-    @Test
-    void shouldThrowErrorOnInvalidUserDetails() {
-        
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            new User("", "Nom", "Prénom", "email@domain.com");
-        });
-        assertEquals("Le matricule ne peut pas être nul ou vide.", exception.getMessage());
-    }
 }
 
 
