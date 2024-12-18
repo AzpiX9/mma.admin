cee96194976e2f7f229b05152c6b89ed9b8cc422
q210138 <a.porcu@student.helmo.be>
2024-12-18 15:05:22 +0100

    extends from Exception not R-E

--- domains/src/main/java/org/helmo/mma/admin/domains/exceptions/UserException.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/exceptions/UserException.java
@@ -1,6 +1,6 @@
 package org.helmo.mma.admin.domains.exceptions;
 
-public class UserException extends RuntimeException {
+public class UserException extends Exception {
     public UserException(String message) {
         super(message);
     }
