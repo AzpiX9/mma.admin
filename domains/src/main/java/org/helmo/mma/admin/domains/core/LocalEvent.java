ed9f0d6ae16501219a05823d23892c6b97ff960d
q210138 <a.porcu@student.helmo.be>
2024-12-18 14:56:40 +0100

    added checking methods

--- domains/src/main/java/org/helmo/mma/admin/domains/core/LocalEvent.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/core/LocalEvent.java
@@ -1,7 +1,17 @@
 package org.helmo.mma.admin.domains.core;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public record LocalEvent(String Username, String Location, LocalDate DateJour, LocalTime Debut, LocalTime Fin, String Summary) {
+    public boolean isOccupied(LocalDateTime timeGiven){
+        return LocalDateTime.of(DateJour,Debut).equals(timeGiven)
+                || timeGiven.isAfter(LocalDateTime.of(DateJour,Debut))
+                && timeGiven.isBefore(LocalDateTime.of(DateJour,Fin));
+    }
+
+    public boolean isBeforeBegin(LocalTime timeGiven){
+        return Debut.isBefore(timeGiven);
+    }
 }
