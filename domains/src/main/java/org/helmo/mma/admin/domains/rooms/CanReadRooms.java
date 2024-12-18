ac7e553b5a187916bc49dc01f9d313e5deba1854
q210138 <a.porcu@student.helmo.be>
2024-12-18 15:02:10 +0100

    method can throw exception

--- domains/src/main/java/org/helmo/mma/admin/domains/rooms/CanReadRooms.java
+++ domains/src/main/java/org/helmo/mma/admin/domains/rooms/CanReadRooms.java
@@ -1,11 +1,12 @@
 package org.helmo.mma.admin.domains.rooms;
 
 import org.helmo.mma.admin.domains.core.Room;
+import org.helmo.mma.admin.domains.exceptions.RoomException;
 
 import java.util.List;
 
 
 public interface CanReadRooms {
     List<Room> getRooms();
-    Room getRoom(String roomId); //TODO: déplacer dans un objet métier
+    Room getRoom(String roomId) throws RoomException; //TODO: déplacer dans un objet métier
 }
